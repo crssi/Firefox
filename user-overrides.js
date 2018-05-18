de246f4f9a6c6e79669f8f6c74c59b9eed382275
crssi <herbert@knavs.net>
2018-05-18 18:18:21 +0200

    Sync - 02bac31

--- user-overrides.js
+++ user-overrides.js
@@ -1,5 +1,5 @@
 /******
-INFO:      Supplement for ghacks-user.js; 15.5.2017 (commit: e5d23f6); https://github.com/ghacksuserjs/ghacks-user.js
+INFO:      Supplement for ghacks-user.js; 16.5.2017 (commit: 02bac31); https://github.com/ghacksuserjs/ghacks-user.js
 IMPORTANT: Read https://github.com/ghacksuserjs/ghacks-user.js/wiki/1.3-Implementation
            Extensions under Essential section are... Essential !!!
 SEE ALSO:  https://github.com/ghacksuserjs/ghacks-user.js/wiki/4.1.1-Setting-Extension-Permission-Exceptions
