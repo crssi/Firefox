2d6660181a91856b529946911eeabb39a41e1313
crssi <herbert@knavs.net>
2018-05-20 06:09:54 +0200

    Containerise is interesting, but not yet

--- user-overrides.js
+++ user-overrides.js
@@ -51,7 +51,6 @@ EXTENSIONS:
 	UNDER INVESTIGATION:
 		Change Timezone (Time Shift); https://addons.mozilla.org/firefox/addon/change-timezone-time-shift/
 
-		Containerise; https://addons.mozilla.org/firefox/addon/containerise/
 		Privacy Badger; https://www.eff.org/files/privacy-badger-latest.xpi
 		Privacy Possum; https://addons.mozilla.org/firefox/addon/privacy-possum/
 		retire.js; https://addons.mozilla.org/firefox/addon/retire-js/
