0950b125e5a3f98b00ff748237b505ea2d8212ba
crssi <herbert@knavs.net>
2018-05-28 22:04:00 +0200

    WE TC re-config

--- user-overrides.js
+++ user-overrides.js
@@ -96,9 +96,6 @@ EXTENSIONS:
 				Multi-Account Containers
   				Open new Temporary Containers if a Permanent Container Tab tries to load a Domain that isn't assigned to "Always open in" that container: Enabled
   				Click [Save]
-?  		Advanced
-?  			General
-?  				[PERSONAL CHOICE] Instead of creating a new Tab replace the currently active Tab: Check
 
 	PERSONAL CHOICE:
 		Copy As Text (Copy As Plain Text); https://addons.mozilla.org/firefox/addon/copy-as-text/
