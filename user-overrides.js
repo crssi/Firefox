a25f925f06143beaac549b7e7aa3891a06d04562
crssi <herbert@knavs.net>
2018-05-31 06:14:20 +0200

    ghacks v60 + overrides

--- user-overrides.js
+++ user-overrides.js
@@ -151,7 +151,7 @@ EXTENSIONS:
 	/* 2682  */ user_pref("security.csp.experimentalEnabled", false);
 	/* 4001  */ user_pref("privacy.firstparty.isolate", false); // true breaks Cookie AutoDelete and some login pages
 	/* 5000  */ user_pref("dom.disable_open_during_load", false); // enable popup
-	// /* 5000  */ user_pref("security.sandbox.content.level", 4); // sandboxie.com doesn't work with FF 60 (default = 5)
+	/* 5000  */ user_pref("security.sandbox.content.level", 4); // sandboxie.com doesn't work with FF 60 (default = 5)
 
 /*** UPDATE ***/
 	/* 0300  */ user_pref("app.update.checkInstallTime.days", 0); // FF update check every day
