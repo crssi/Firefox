f974fa281bb00f1ececa6fb30d3f97aed73a14fa
crssi <herbert@knavs.net>
2018-06-05 08:45:01 +0200

    Sync - a4a9b9a

--- user-overrides.js
+++ user-overrides.js
@@ -1,6 +1,6 @@
 /******
 HOME:      https://github.com/crssi/Firefox
-INFO:      Supplement for ghacks-user.js; 1.6.2017 (commit: c61e633); https://github.com/ghacksuserjs/ghacks-user.js
+INFO:      Supplement for ghacks-user.js; 4.6.2017 (commit: a4a9b9a); https://github.com/ghacksuserjs/ghacks-user.js
 IMPORTANT: Read https://github.com/ghacksuserjs/ghacks-user.js/wiki/1.3-Implementation/
            Extensions under Essential section are... Essential !!!
 SEE ALSO:  https://support.mozilla.org/en-US/kb/profiles-where-firefox-stores-user-data/
