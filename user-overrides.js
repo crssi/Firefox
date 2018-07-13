d5c37ed30cea0805fada2d15031e500e678e462c
crssi <herbert@knavs.net>
2018-07-13 11:47:48 +0200

    Stuff to remove soon marked as -*/

--- user-overrides.js
+++ user-overrides.js
@@ -116,7 +116,7 @@ EXTENSIONS:
 ******/
 
 /*** PERSONAL TWEAKS ***/
-	// /* 0102  */ user_pref("browser.startup.page", 0); // set blank page at start up
+	// /* 0102 -*/ user_pref("browser.startup.page", 0); // set blank page at start up
 	/* 0862  */ user_pref("places.history.enabled", false); // disable history, but can also be cleared when exiting FF, see 2803
 	/* 0515  */ user_pref("extensions.screenshots.disabled", true); // disable screenshots
 	/* 0701  */ user_pref("network.dns.disableIPv6", true); // disable IPv6
@@ -146,18 +146,18 @@ EXTENSIONS:
 
 /*** BREAKAGE TWEAKS ***/
 	/* 0001  */ user_pref("browser.privatebrowsing.autostart", false); // disable PB
-	// /* 1202  */ user_pref("security.tls.version.min", 1); // some SSL pages still uses TLS 1.0 and breaks with the value higher than 1 - testing value of 2
+	// /* 1202 -*/ user_pref("security.tls.version.min", 1); // some SSL pages still uses TLS 1.0 and breaks with the value higher than 1 - testing value of 2
 	/* 1212  */ user_pref("security.OCSP.require", false); // OCSP soft fail
 	/* 1241  */ user_pref("security.mixed_content.block_display_content", false); // allow secure and non-secure content
 	/* 2030  */ user_pref("media.autoplay.enabled", true); // false introduces funny things
 	/* 2206  */ user_pref("full-screen-api.enabled", true); // false breaks fullscreen
-	// /* 2402  */ user_pref("dom.event.clipboardevents.enabled", true); // enable copy/cut/paste
+	// /* 2402 -*/ user_pref("dom.event.clipboardevents.enabled", true); // enable copy/cut/paste
 	/* 2510  */ user_pref("dom.webaudio.enabled", true); // should be false except if using WE "AudioContext Fingerprint Defender" or simmilar
 	/* 2662  */ user_pref("extensions.webextensions.restrictedDomains", ""); // allow extensions on mozilla domains
 	/* 2682  */ user_pref("security.csp.experimentalEnabled", false);
 	/* 4001??*/ user_pref("privacy.firstparty.isolate", false); // true breaks Cookie AutoDelete and some login pages
-	// /* 5000  */ user_pref("dom.disable_open_during_load", false); // enable popup
-	/* 5000  */ user_pref("security.sandbox.content.level", 5); // sandboxie.com doesn't work with FF 60 (default = 5); resolved in Sandboxie 5.26
+	// /* 5000 -*/ user_pref("dom.disable_open_during_load", false); // enable popup
+	/* 5000 -*/ user_pref("security.sandbox.content.level", 5); // sandboxie.com doesn't work with FF 60 (default = 5); resolved in Sandboxie 5.26
 
 /*** UPDATE ***/
 	/* 0300  */ user_pref("app.update.checkInstallTime.days", 0); // FF update check every day
