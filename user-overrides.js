90bb293e72aa72e42f0ce2b73e2703ffa819c517
crssi <herbert@knavs.net>
2018-07-18 10:16:01 +0200

    user_pref("security.tls.version.min", 1);
    Still too soon for higher value
    racunalniske-novice...
    uvoz-izvoz.posta...

--- user-overrides.js
+++ user-overrides.js
@@ -145,7 +145,7 @@ EXTENSIONS:
 
 /*** BREAKAGE TWEAKS ***/
 	/* 0001  */ user_pref("browser.privatebrowsing.autostart", false); // disable PB
-	// /* 1202 -*/ user_pref("security.tls.version.min", 1); // some SSL pages still uses TLS 1.0 and breaks with the value higher than 1 - testing value of 2
+	/* 1202 -*/ user_pref("security.tls.version.min", 1); // some SSL pages still uses TLS 1.0 and breaks with the value higher than 1
 	/* 1212  */ user_pref("security.OCSP.require", false); // OCSP soft fail
 	/* 1241  */ user_pref("security.mixed_content.block_display_content", false); // allow secure and non-secure content
 	/* 2030  */ user_pref("media.autoplay.enabled", true); // false introduces funny things
