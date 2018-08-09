28fc6c4d536156a0b565d2d008e1446297574ad7
crssi <herbert@knavs.net>
2018-08-09 10:47:31 +0200

    Investigate user_pref("browser.download.forbid_open_with", false)

--- user-overrides.js
+++ user-overrides.js
@@ -149,6 +149,7 @@ EXTENSIONS:
 	/* 2206  */ user_pref("full-screen-api.enabled", true); // false breaks fullscreen
 	/* 2403  */ user_pref("dom.allow_cut_copy", true); // !!! Investigate - to decide
 	/* 2510  */ user_pref("dom.webaudio.enabled", true); // should be false except if using WE "AudioContext Fingerprint Defender" or simmilar
+	/* 2654  */ user_pref("browser.download.forbid_open_with", false); // !!! Investigate - enable to open download content
 	/* 2662  */ user_pref("extensions.webextensions.restrictedDomains", ""); // allow extensions on mozilla domains
 	/* 2682  */ user_pref("security.csp.experimentalEnabled", false);
 	/* 4001 ?*/ user_pref("privacy.firstparty.isolate", false); // true breaks Cookie AutoDelete and some login pages
