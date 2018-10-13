a7d47ab7d169a75fa3bf5e4896abcb86f0c98340
crssi <herbert@knavs.net>
2018-10-13 19:26:08 +0200

    nits

--- user-overrides.js
+++ user-overrides.js
@@ -157,7 +157,7 @@ youtube.com/embed/
 
 /*** BREAKAGE TWEAKS ***/
 	/* 0001  */ user_pref("browser.privatebrowsing.autostart", false); // disable PB
-  /* 1201  */ user_pref("security.ssl.require_safe_negotiation", true); // disable old SSL/TLS - vulnerable to a MiTM attack
+	/* 1201  */ user_pref("security.ssl.require_safe_negotiation", true); // disable old SSL/TLS - vulnerable to a MiTM attack
 	/* 1240  */ user_pref("security.mixed_content.block_active_content", false); // allow secure and non-secure content
 	/* 1241  */ user_pref("security.mixed_content.block_display_content", false); // allow secure and non-secure content
 	/* 1603  */ user_pref("network.http.referer.XOriginPolicy", 0); // should be 1, except when spoofing by 3rd party Smart Referer WE
