d0a84015ce1c4b4bb8e9f6a27bd6f5a49d71e850
crssi <herbert@knavs.net>
2018-12-04 09:25:06 +0100

    Sync

--- user-overrides.js
+++ user-overrides.js
@@ -1,6 +1,6 @@
 /******
 HOME:      https://github.com/crssi/Firefox
-INFO:      Supplement for ghacks-user.js; 29.11.2018 (commit: db56940); https://github.com/ghacksuserjs/ghacks-user.js
+INFO:      Supplement for ghacks-user.js; 4.12.2018 (commit: 834857b); https://github.com/ghacksuserjs/ghacks-user.js
 NOTE:      Before proceeding further, make a backup of your current profile
 
 1. Download user.js from https://raw.githubusercontent.com/ghacksuserjs/ghacks-user.js/master/user.js and place it into "profile folder"
