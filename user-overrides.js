1f490194aa4aae0ec52c23a6bde4183d26378cbf
crssi <herbert@knavs.net>
2018-12-05 17:54:49 +0100

    Redirect AMP to HTML WE back to operational

--- user-overrides.js
+++ user-overrides.js
@@ -44,6 +44,7 @@ ESSENTIAL EXTENSIONS:
     URL request types: font, image, imageset, main_frame, media, object, object_subrequest, script, stylesheet, sub_frame, websocket, xbl, xml_dtd, xmlhttprequest, xslt, other
     Click [Save preferences]
   Privacy-Oriented Origin Policy; https://addons.mozilla.org/firefox/addon/privacy-oriented-origin-policy/ (https://github.com/claustromaniac/poop/)
+  Redirect AMP to HTML; https://addons.mozilla.org/firefox/addon/amp2html/ (https://github.com/da2x/amp2html/)
   Skip Redirect; https://addons.mozilla.org/firefox/addon/skip-redirect/ (https://github.com/sblask/webextension-skip-redirect/)
     Blacklist (Append, without indentation to simplify copy/paste):
 _sso=
@@ -153,7 +154,6 @@ EXPERIMENTAL/INVESTIGATION:
     Force value of access-control-allow-origin: empty
     Activation whitelist: empty
     Remember to save your changes: Click [Save]
-  Redirect AMP to HTML; https://addons.mozilla.org/firefox/addon/amp2html/ (https://github.com/da2x/amp2html/)
 
 ******/
 
