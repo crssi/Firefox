65dc2696f8a7968c19d8db24d3348b105258a6ed
crssi <herbert@knavs.net>
2019-02-09 12:02:37 +0100

    Update user-overrides.js

--- user-overrides.js
+++ user-overrides.js
@@ -1,6 +1,6 @@
 /******
 HOME:      https://github.com/crssi/Firefox
-INFO:      Supplement for ghacks-user.js; 8.2.2019 (commit: de0ebbe); https://github.com/ghacksuserjs/ghacks-user.js
+INFO:      Supplement for ghacks-user.js; 9.2.2019 (commit: 5c703f0); https://github.com/ghacksuserjs/ghacks-user.js
 NOTE:      Before proceeding further, make a backup of your current profile
 
 0. Download user.js from https://raw.githubusercontent.com/ghacksuserjs/ghacks-user.js/master/user.js and place it into "profile folder"
