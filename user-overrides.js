4833e36dd9453c2940ed3ecfbe66a5b5f11330d0
crssi <herbert@knavs.net>
2019-03-26 10:27:59 +0100

    Update user-overrides.js

--- user-overrides.js
+++ user-overrides.js
@@ -183,6 +183,7 @@ USEFUL/INTERESTING EXTENSIONS:
   /* 5000  */ user_pref("browser.tabs.closeWindowWithLastTab", false); // do not close FF with the last tab
   /* 5000  */ user_pref("browser.tabs.loadBookmarksInTabs", true); // open bookmarks in a new tab
   /* 5000  */ user_pref("browser.urlbar.decodeURLsOnCopy", true); // see  Bugzilla 1320061
+  /* 5000  */ user_pref("general.warnOnAboutConfig", false); // remove about:config warning
   /* 5000  */ user_pref("ui.key.menuAccessKey", 0); // disable alt key toggling the menu bar
   /* 5000x */ user_pref("findbar.highlightAll", true); // hihglight all hits on search
   /* 5002  */ user_pref("full-screen-api.warning.timeout", 0); // remove fullscreen message annoyance
