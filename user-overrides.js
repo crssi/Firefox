cb6bf7701f421f4b0d304b61c83714e1cd4c3fac
crssi <herbert@knavs.net>
2019-07-02 22:25:24 +0200

    Update user-overrides.js

--- user-overrides.js
+++ user-overrides.js
@@ -139,7 +139,7 @@ USEFUL/INTERESTING EXTENSIONS:
   /* 0850d */ user_pref("browser.urlbar.autoFill", false); // disable location bar autofill
 
 /*** ENTERPRISE USE SPECIFIC ***/
-  /* 0000  */ user_pref("security.enterprise_roots.enabled", true); // use OS certificates store
+  /*?0000  */ user_pref("security.enterprise_roots.enabled", true); // use OS certificates store, Firefox 68+
   /* 0000  */ user_pref("network.automatic-ntlm-auth.allow-non-fqdn", true); // enable SSO for hostnames
   // /* 0000  */ user_pref("network.negotiate-auth.trusted-uris", ""); // enable SSO for comma separated domain list
   // /* 0000  */ user_pref("network.automatic-ntlm-auth.trusted-uris", ""); // enable SSO for comma separated domain list
