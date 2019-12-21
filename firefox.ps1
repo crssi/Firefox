ec32c1300b1da2eec5a6861e10ce9a6e887fd726
crssi <herbert@knavs.net>
2019-12-21 20:32:52 +0100

    " > '

--- firefox.ps1
+++ firefox.ps1
@@ -3,7 +3,7 @@ do { Start-Sleep -Milliseconds 500 } while ((Get-Process -Name 'firefox' -ErrorA
 
 $tmpFile = New-TemporaryFile
 Remove-Item -Path $tmpFile
-$tmpFolder = New-Item -Path $tmpFile.DirectoryName -Name $tmpFile.Name -ItemType "directory"
+$tmpFolder = New-Item -Path $tmpFile.DirectoryName -Name $tmpFile.Name -ItemType 'directory'
 Remove-Variable -Name tmpFile
 
 Import-Module -Name BitsTransfer
@@ -15,11 +15,11 @@ Get-Content -Path "$tmpFolder\Firefox\installs.ini" | ForEach-Object { if ($_.St
 Get-Content -Path "$($env:APPDATA)\Mozilla\Firefox\installs.ini" | ForEach-Object { if ($_.StartsWith('Default=Profiles/')) { $oldProfilePath = "$($env:APPDATA)\Mozilla\Firefox\Profiles\$($_.Replace('Default=Profiles/', ''))" } }
 $tmpProfilePath = "$tmpFolder\Firefox\Profiles\$($newProfilePath.split('\')[-1])"
 
-$userProfileFiles = @("cert9.db","content-prefs.sqlite","favicons.sqlite","handlers.json","key4.db","logins.json","permissions.sqlite","persdict.dat","pkcs11.txt","places.sqlite")
+$userProfileFiles = @('cert9.db','content-prefs.sqlite','favicons.sqlite','handlers.json','key4.db','logins.json','permissions.sqlite','persdict.dat','pkcs11.txt','places.sqlite')
 $userProfileFiles | ForEach-Object { Copy-Item -Path "$oldProfilePath\$_" -Destination "$tmpProfilePath\$_" -Force -ErrorAction SilentlyContinue }
 
-Compress-Archive -Path "$($env:APPDATA)\Mozilla\Firefox\*" -DestinationPath "$($env:APPDATA)\Mozilla\Firefox_Profile_Backup-$((Get-Date).ToString("yyyy.MM.dd_HH.mm.ss")).zip"
-Write-Verbose -Message "Backup: $($env:APPDATA)\Mozilla\Firefox_Profile_Backup-$((Get-Date).ToString("yyyy.MM.dd_HH.mm.ss")).zip"
+Compress-Archive -Path "$($env:APPDATA)\Mozilla\Firefox\*" -DestinationPath "$($env:APPDATA)\Mozilla\Firefox_Profile_Backup-$((Get-Date).ToString('yyyy.MM.dd_HH.mm.ss')).zip"
+Write-Verbose -Message "Backup: $($env:APPDATA)\Mozilla\Firefox_Profile_Backup-$((Get-Date).ToString('yyyy.MM.dd_HH.mm.ss')).zip"
 
 Remove-Item -Path "$($env:APPDATA)\Mozilla\Firefox" -Recurse -Force
 
