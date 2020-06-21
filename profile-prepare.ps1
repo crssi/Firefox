39a19b786b022112ec94e65d227950bcb5f34748
crssi <herbert@knavs.net>
2020-06-21 21:13:40 +0200

    update

--- profile-prepare.ps1
+++ profile-prepare.ps1
@@ -3,16 +3,17 @@ $Utf8NoBomEncoding = New-Object System.Text.UTF8Encoding $False
 
 $files = @('extensions.json','pkcs11.txt','compatibility.ini','pluginreg.dat')
 forEach ($file in $files) {
-    $data = Get-Content -Encoding UTF8 -Path $profilePath\$file
-    $data = $data.Replace($env:APPDATA.Replace('\','/').Replace(' ','%20'),'%appdata%')
-    $data = $data.Replace($env:APPDATA.Replace('\','\\'),'%appdata%')
-    $data = $data.Replace($env:ProgramFiles.Replace('\','/').Replace(' ','%20'),'%programfiles%')
-    $data = $data.Replace($env:ProgramFiles.Replace('\','\\'),'%programfiles%')
-    $data = $data.Replace($env:ProgramFiles,'%programfiles%')
-    [System.IO.File]::WriteAllLines("$profilePath\$file", $data, $Utf8NoBomEncoding)
+    $content = Get-Content -Encoding UTF8 -Path $profilePath\$file
+    $content = $content.Replace($env:APPDATA.Replace('\','/').Replace(' ','%20'),'%appdata%')
+    $content = $content.Replace($env:APPDATA.Replace('\','\\'),'%appdata%')
+    $content = $content.Replace($env:ProgramFiles.Replace('\','/').Replace(' ','%20'),'%programfiles%')
+    $content = $content.Replace($env:ProgramFiles.Replace('\','\\'),'%programfiles%')
+    $content = $content.Replace($env:ProgramFiles,'%programfiles%')
+    [System.IO.File]::WriteAllLines("$profilePath\$file", $content, $Utf8NoBomEncoding)
 }
 
 Get-ChildItem $profilePath\storage\default | where { $_.name -notmatch '\^userContextId' } | Remove-Item -Force
 Get-ChildItem $profilePath\addonStartup.json.lz4 | Remove-Item -Force
+Get-ChildItem $profilePath\pkcs11.txt | Remove-Item -Force
 
 Compress-Archive -Path "$($env:APPDATA)\Mozilla\Firefox\*" -DestinationPath "$env:UserProfile\Documents\GitHub\Firefox\Profile.zip" -CompressionLevel Optimal -Force
