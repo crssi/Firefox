ccab66300eac126e5a61c903f64b031824e9de65
crssi <herbert@knavs.net>
2020-07-07 11:07:09 +0200

    Update profile-create.ps1

--- .Tools/profile-create.ps1
+++ .Tools/profile-create.ps1
@@ -1,19 +1,17 @@
 # https://github.com/avih/dejsonlz4
 Get-Content -Path "$($env:APPDATA)\Mozilla\Firefox\installs.ini" | ForEach-Object { if ($_.StartsWith('Default=Profiles/')) { $profilePath = "$($env:APPDATA)\Mozilla\Firefox\Profiles\$($_.Replace('Default=Profiles/', ''))" } }
-$Utf8NoBomEncoding = New-Object System.Text.UTF8Encoding $False
 
 & "$env:UserProfile\Documents\GitHub\Firefox\.Tools\dejsonlz4.exe" @("$profilePath\addonStartup.json.lz4","$profilePath\addonStartup.json")
 Copy-Item "$env:UserProfile\Documents\GitHub\Firefox\.Tools\jsonlz4.exe" $profilePath -Force
 
-$files = @('extensions.json','pkcs11.txt','compatibility.ini','pluginreg.dat','addonStartup.json')
-forEach ($file in $files) {
+forEach ($file in @('extensions.json','pkcs11.txt','compatibility.ini','pluginreg.dat','addonStartup.json')) {
     $content = Get-Content -Encoding UTF8 -Path $profilePath\$file
     $content = $content.Replace($env:APPDATA.Replace('\','/').Replace(' ','%20'),'%appdata%')
     $content = $content.Replace($env:APPDATA.Replace('\','\\'),'%appdata%')
     $content = $content.Replace($env:ProgramFiles.Replace('\','/').Replace(' ','%20'),'%programfiles%')
     $content = $content.Replace($env:ProgramFiles.Replace('\','\\'),'%programfiles%')
     $content = $content.Replace($env:ProgramFiles,'%programfiles%')
-    [System.IO.File]::WriteAllLines("$profilePath\$file", $content, $Utf8NoBomEncoding)
+    [System.IO.File]::WriteAllLines("$profilePath\$file", $content, $(New-Object System.Text.UTF8Encoding $False))
 }
 
 Get-ChildItem $profilePath\storage\default | where { $_.name -notmatch '\^userContextId' } | Remove-Item -Recurse -Force -Confirm:$false
