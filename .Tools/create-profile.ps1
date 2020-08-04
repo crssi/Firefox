53ed613ca100a52de4310772c427a3f4739f8795
crssi <herbert@knavs.net>
2020-08-04 15:12:02 +0200

    try catch

--- .Tools/create-profile.ps1
+++ .Tools/create-profile.ps1
@@ -7,17 +7,19 @@ Copy-Item "$env:UserProfile\Documents\GitHub\Firefox\.Tools\jsonlz4.exe" $profil
 forEach ($file in @('cert9.db','content-prefs.sqlite','favicons.sqlite','handlers.json','key4.db','logins.json','permissions.sqlite','persdict.dat','pkcs11.txt','places.sqlite')) { Remove-Item -Path $profilePath\$file -Force -ErrorAction SilentlyContinue }
 
 forEach ($file in @('extensions.json','compatibility.ini','pluginreg.dat','addonStartup.json')) {
-    $content = Get-Content -Encoding UTF8 -Path $profilePath\$file
-    $content = $content.Replace($env:APPDATA.Replace('\','/').Replace(' ','%20'),'%appdata%')
-    $content = $content.Replace($env:APPDATA.Replace('\','\\'),'%appdata%')
-    $content = $content.Replace($env:ProgramFiles.Replace('\','/').Replace(' ','%20'),'%programfiles%')
-    $content = $content.Replace($env:ProgramFiles.Replace('\','\\'),'%programfiles%')
-    $content = $content.Replace($env:ProgramFiles,'%programfiles%')
-    [System.IO.File]::WriteAllLines("$profilePath\$file", $content, $(New-Object System.Text.UTF8Encoding $False))
+  $content = Get-Content -Encoding UTF8 -Path $profilePath\$file
+  $content = $content.Replace($env:APPDATA.Replace('\','/').Replace(' ','%20'),'%appdata%')
+  $content = $content.Replace($env:APPDATA.Replace('\','\\'),'%appdata%')
+  $content = $content.Replace($env:ProgramFiles.Replace('\','/').Replace(' ','%20'),'%programfiles%')
+  $content = $content.Replace($env:ProgramFiles.Replace('\','\\'),'%programfiles%')
+  $content = $content.Replace($env:ProgramFiles,'%programfiles%')
+  [System.IO.File]::WriteAllLines("$profilePath\$file", $content, $(New-Object System.Text.UTF8Encoding $False))
 }
 
-Get-ChildItem $profilePath\storage\default | where { $_.name -notmatch '\^userContextId' } | Remove-Item -Recurse -Force -Confirm:$false
-Get-ChildItem $profilePath\addonStartup.json.lz4 | Remove-Item -Force
-Get-ChildItem $profilePath\pkcs11.txt | Remove-Item -Force
+try {
+  Get-ChildItem $profilePath\storage\default | where { $_.name -notmatch '\^userContextId' } | Remove-Item -Recurse -Force -Confirm:$false
+  Get-ChildItem $profilePath\addonStartup.json.lz4 | Remove-Item -Force
+  Get-ChildItem $profilePath\pkcs11.txt | Remove-Item -Force
+} Catch { }
 
 Compress-Archive -Path "$($env:APPDATA)\Mozilla\Firefox\*" -DestinationPath "$env:UserProfile\Documents\GitHub\Firefox\Profile.zip" -CompressionLevel Optimal -Force
