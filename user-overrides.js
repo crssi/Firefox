d33f3583e2d4bb8079fbbfc96f5c3bb0adce98e6
crssi <herbert@knavs.net>
2021-09-20 21:15:18 +0200

    minecraft login

--- user-overrides.js
+++ user-overrides.js
@@ -1,6 +1,6 @@
 /******
 HOME: https://github.com/crssi/Firefox
-INFO: Supplement for arkenfox user.js; https://github.com/arkenfox/user.js; inline with commit 76c1aad on 10.9.2021
+INFO: Supplement for arkenfox user.js; https://github.com/arkenfox/user.js; inline with commit 1c6d633 on 11.9.2021
 NOTE: Before proceeding further, make a backup of your current profile
 
 1. Download user.js from https://raw.githubusercontent.com/arkenfox/user.js/master/user.js, append this file and place it into "profile folder"
