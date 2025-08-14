74b008efe2516dd5898323b9f39edb5ddad70ec2
crssi <herbert@knavs.net>
2025-08-14 08:27:31 +0200

    v140

--- user-overrides.js
+++ user-overrides.js
@@ -1,6 +1,6 @@
 /******
 HOME: https://github.com/crssi/Firefox
-INFO: Supplement for arkenfox user.js; https://github.com/arkenfox/user.js; inline with commit 3d76c74 on 6.3.2025
+INFO: Supplement for arkenfox user.js; https://github.com/arkenfox/user.js; inline with commit 9e8ead8 on 14.8.2025
 NOTE: Before proceeding further, make a backup of your current profile
 
 1. Download user.js from https://raw.githubusercontent.com/arkenfox/user.js/master/user.js, append this file and place it into "profile folder"
